bd2b8f7634c0934da175b46711d3c04d61384a2a
Furkan Akif ISLEK <furkanakifislek@gmail.com>
2022-09-22 21:23:05 +0300

    template commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
-import './App.css';
-import About from './components/About/About';
-import Header from './components/Header/Header';
-
-function App() {
-  return (
-    <div className="App">
-      
-      <Header/>
-      <About/>
-      
-    </div>
-  );
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/resumeData.js
@@ -0,0 +1,108 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Furkan Akif ISLEK",
+    "role": "Frontend Developer",
+    "linkedinId":"https://www.linkedin.com/in/furkanislek/",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/furkanislek/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/furkanislek",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"http://twitter.com/furukderki",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"Hi there,I am contacting with you for working as a intern frontend developer in your company.I graduated from Cukurova University, Civil Engineering at 2020. After working for a year as a civil engineer in different companies. I decided to change my career to be a frontend developer.  I am improving my frontend skills for last 8 months. Currently, I am looking for an opportunity to work as a frontend developer.In last 8 months, I focused Javascript and especially ReactJS.",
+    "address":"Gaziantep, Turkey",
+    "website":"furkanakifislek@gmail.com",
+    "education":[
+      {
+        "UniversityName":"Cukurova University",
+        "specialization":"Civil Engineering",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Nuhoglu Insaat San. ve Tic. A.S.",
+        "specialization":"Civil Engineer",
+        "MonthOfLeaving":"March",
+        "YearOfLeaving":"2022",
+      },
+      {
+        "CompanyName":"Yesil Enerji Enerji Verimliligi Danısmanlık Sanayi Ve Ticaret Limited Sirketi",
+        "specialization":"Civil Engineer",
+        "MonthOfLeaving":"October",
+        "YearOfLeaving":"2021",
+      }
+    ],
+    "skills":[
+      {
+        "skillname" : "JavaScript"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname" : "Photoshop"
+      },
+      {
+        "skillname" : "Wordpress"
+      },
+      {
+        "skillname" : "Seo"
+      },
+      
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
